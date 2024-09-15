cd731e7df49b91d764064033a959882c08709b46
Alexander Sullivan <alexander.j.sullivan@hotmail.com>
2024-09-15 11:35:07 -0400

    [fix][pwa] Fixed issues relating to PWA and Service Workers not working as intended (#57)
    Fixed multiple issues related to the PWA and Service Workers not working
    as intended:
    
    - Added missing offline support to service workers
    - Fixed service workers trying to grab files that don't exist
    - Fixed missing form factor in web manifest screenshots
    - Fixed typo
    - Updated packages

--- workbox-config.js
+++ workbox-config.js
@@ -12,21 +12,54 @@ module.exports = {
 		'node_modules/**/*',
 	],
 	// Define runtime caching rules.
-	"runtimeCaching": [{
-		// Match any request that ends with .png, .jpg, .jpeg, .webp, .ico or .svg
-		"urlPattern": /\.(?:png|jpg|jpeg|webp|ico|svg)$/,
-
-		// Apply a cache-first strategy.
-		"handler": 'CacheFirst',
-
-		"options": {
-			// Use a custom cache name.
-			"cacheName": 'images',
-
-			// Only cache 10 images.
-			"expiration": {
-				"maxEntries": 10,
+	"runtimeCaching": [
+		{
+			// Match any request that ends with .png, .jpg, .jpeg, .webp, .ico or .svg
+			"urlPattern": /\.(?:png|jpg|jpeg|webp|ico|svg)$/,
+			// Apply a cache-first strategy.
+			"handler": 'CacheFirst',
+			"options": {
+				// Use a custom cache name.
+				"cacheName": 'images',
+				// Only cache 10 images.
+				"expiration": {
+					"maxEntries": 10,
+				},
 			},
 		},
-	}],
+		{
+			// Match HTML files for offline support
+			"urlPattern": /\.html$/,
+			// Apply a network-first strategy.
+			"handler": 'NetworkFirst',
+			"options": {
+				// Use a custom cache name.
+				"cacheName": 'html',
+				// Configure fallback for offline usage
+				"networkTimeoutSeconds": 10,
+				"expiration": {
+					"maxEntries": 50,
+				},
+				"cacheableResponse": {
+					"statuses": [0, 200],
+				},
+			},
+		},
+		{
+			// Match CSS and JS files for offline support
+			"urlPattern": /\.(?:css|js)$/,
+			// Apply a network-first strategy.
+			"handler": 'NetworkFirst',
+			"options": {
+				// Use a custom cache name.
+				"cacheName": 'assets',
+				"expiration": {
+					"maxEntries": 50,
+				},
+				"cacheableResponse": {
+					"statuses": [0, 200],
+				},
+			},
+		}
+	],
 };
